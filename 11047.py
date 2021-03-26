02668d34df108e9561d08fc56f42c88530955b45
rrrrim <57060716+rrrrim@users.noreply.github.com>
2021-03-26 12:17:55 +0900

    Update 11047.py

--- 11047.py
+++ 11047.py
@@ -11,7 +11,18 @@
 
 n, k = map(int, input().split())
 coin = []
+cnt = 0
 for i in range(n):
     c = int(input())
     coin.append(c)
-#진심 헐랭방~~꾸
+coin.reverse()
+for i in range(n):
+    if k == 0:
+        break
+    else:
+        if k >= coin[i]:
+            cnt += k // coin[i]
+            k %= coin[i]
+print(cnt)
+
+
